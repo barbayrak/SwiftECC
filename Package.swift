7c595609e5b2733c00025a3b812cbcf1db85477d
Kaan <kaanbarisbayrak@gmail.com>
2022-06-18 14:29:52 +0200

    Dependency changes

--- Package.swift
+++ Package.swift
@@ -13,15 +13,15 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/leif-ibsen/ASN1", from: "2.0.2"),
-        .package(url: "https://github.com/leif-ibsen/BigInt", from: "1.4.0"),
+        .package(url: "https://github.com/barbayrak/ASN1", from: "3.0.0"),
+        .package(url: "https://github.com/barbayrak/BInt", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftECC",
-            dependencies: ["ASN1", "BigInt"]),
+            dependencies: ["ASN1", "BInt"]),
         .testTarget(
             name: "SwiftECCTests",
             dependencies: ["SwiftECC"]),
